90cc64287a1489aa9a5ef6cc718b6da242f490d6
jawwwr <jarielb96@gmail.com>
2019-09-09 01:38:15 +0800

    Implement adding of person to app

--- app/containers/PersonAdd/constants.js
+++ app/containers/PersonAdd/constants.js
@@ -5,3 +5,14 @@
  */
 
 export const DEFAULT_ACTION = 'app/PersonAdd/DEFAULT_ACTION';
+export const PERSONAL_CHANGE_INPUT = 'wmr-cnt/PersonAdd/PERSONAL_CHANGE_INPUT';
+export const PERSONAL_CHANGE_SELECT =
+  'wmr-cnt/PersonAdd/PERSONAL_CHANGE_SELECT';
+export const PERSON_DATA_CHANGE = 'wmr-cnt/PersonAdd/PERSON_DATA_CHANGE';
+
+export const PERSON_DATA_SAVE = 'wmr-cnt/PersonAdd/PERSON_DATA_SAVE';
+export const PERSON_DATA_SAVE_END = 'wmr-cnt/PersonAdd/PERSON_DATA_SAVE_END';
+export const PERSON_DATA_SAVE_ERROR =
+  'wmr-cnt/PersonAdd/PERSON_DATA_SAVE_ERROR';
+export const PERSON_DATA_SAVE_SUCCESS =
+  'wmr-cnt/PersonAdd/PERSON_DATA_SAVE_SUCCESS';
--- app/containers/PersonAdd/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectPersonAddDomain } from '../selectors';
-
-describe('selectPersonAddDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
